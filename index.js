f9067d6a683a381008c0ec5fbb3383ec4ea3a36f
Julian <black-thunder@gmx.ch>
2017-02-04 21:33:12 +0100

    Update index.js
    use svg steal logo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 
 let content = `
 <div class="content">
-	<img id="logo" src="//stealjs.com/img/steal-landing-page-logo.jpg">
+	<img id="logo" src="//stealjs.com/docs/static/node_modules/bit-docs-docjs-theme/static/img/StealJS-Logo-V2.svg">
 	<p class="quote">Futuristic JavaScript dependency loader and builder</p>
 </div>`;
 
